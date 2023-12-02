f93cc2cb7c04966190bae70cfd73a999795b1a5f
Drew Markel <drewmrk8@gmail.com>
2023-12-02 11:39:37 -0500

    Day 2 part 2 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -54,6 +54,56 @@ fun main() {
         return result
     }
 
+    fun part2(input: List<String>): Int {
+        var result = 0
+
+        for (line in input) {
+            val splitLine = line.split(":")
+
+            var biggestRed = 0
+            var biggestGreen = 0
+            var biggestBlue = 0
+
+            val picks = splitLine[1].split(";")
+            for (colorPicks in picks) {
+                val splitColorPicks = colorPicks.split(",")
+
+                val reds = splitColorPicks.filter { it.contains("red") }
+                val greens = splitColorPicks.filter { it.contains("green") }
+                val blues = splitColorPicks.filter { it.contains("blue") }
+
+                if (reds.isNotEmpty()) {
+                    val red = reds[0].split(" ")[1].toInt()
+
+                    if (red > biggestRed) {
+                        biggestRed = red
+                    }
+                }
+
+                if (greens.isNotEmpty()) {
+                    val green = greens[0].split(" ")[1].toInt()
+
+                    if (green > biggestGreen) {
+                        biggestGreen = green
+                    }
+                }
+
+                if (blues.isNotEmpty()) {
+                    val blue = blues[0].split(" ")[1].toInt()
+
+                    if (blue > biggestBlue) {
+                        biggestBlue = blue
+                    }
+                }
+            }
+
+            result += biggestRed * biggestGreen * biggestBlue
+        }
+
+        return result
+    }
+
     val input = readInput("Day02")
     part1(input).println()
+    part2(input).println()
 }
