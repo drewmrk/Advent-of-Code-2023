c847e1628dea815a581571aa18e0022ef384d486
Drew Markel <drewmrk8@gmail.com>
2023-12-06 17:47:55 -0500

    Day 6 part 2 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,5 +1,5 @@
 fun main() {
-    data class Race(val time: Int, val recordDistance: Int)
+    data class Race(val time: Long, val recordDistance: Long)
 
     fun inputToRaces(input: List<String>): List<Race> {
         val races: MutableList<Race> = mutableListOf()
@@ -7,10 +7,17 @@ fun main() {
         val distances = input[1].split(":")[1].trim().split(" ").filter { it.isNotBlank() }
 
         for (i in times.indices) {
-            races.add(Race(times[i].toInt(), distances[i].toInt()))
+            races.add(Race(times[i].toLong(), distances[i].toLong()))
         }
 
         return races
+    }
+
+    fun inputToRacesCombine(input: List<String>): Race {
+        val time = input[0].split(":")[1].trim().split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+        val distance = input[1].split(":")[1].trim().split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+
+        return Race(time, distance)
     }
 
     fun part1(input: List<String>): Int {
@@ -44,6 +51,22 @@ fun main() {
         return result
     }
 
+    fun part2(input: List<String>): Int {
+        val race = inputToRacesCombine(input)
+
+        var waysToWin = 0
+        for (i in 0..race.time) {
+            val distanceToTravel = (race.time - i) * i
+
+            if (distanceToTravel > race.recordDistance) {
+                waysToWin++
+            }
+        }
+
+        return waysToWin
+    }
+
     val input = readInput("Day06")
     part1(input).println()
+    part2(input).println()
 }
