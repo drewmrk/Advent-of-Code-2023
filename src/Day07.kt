b50538cc94d6e02bf334bde2f4e7339993e1fb0e
Drew Markel <drewmrk8@gmail.com>
2023-12-07 23:00:22 -0500

    Day 7 part 2 solution

--- src/Day07.kt
+++ src/Day07.kt
@@ -19,10 +19,11 @@ fun main() {
         "5" to 4,
         "4" to 3,
         "3" to 2,
-        "2" to 1
+        "2" to 1,
+        "X" to 0
     )
 
-    data class Hand(val cards: String, val bidAmt: Long, val handType: String)
+    data class Hand(var cards: String, val bidAmt: Long, val handType: String, val originalCards: String)
 
     fun determineHandType(cards: String): String {
         val cardAmts: MutableMap<Char, Long> = mutableMapOf()
@@ -35,20 +36,35 @@ fun main() {
             }
         }
 
-        return if (cardAmts.values.contains(5)) {
-            "quintuple"
+        if (cardAmts.values.contains(5)) {
+            return "quintuple"
         } else if (cardAmts.values.contains(4)) {
-            "quadruple"
+            if (cards.contains("J")) return "quintuple"
+
+            return "quadruple"
         } else if (cardAmts.values.contains(3) && cardAmts.values.contains(2)) {
-            "full"
+            if (cards.contains("J")) return "quintuple"
+
+            return "full"
         } else if (cardAmts.values.contains(3)) {
-            "triple"
+            if (cards.contains("J")) return "quadruple"
+
+            return "triple"
         } else if (cardAmts.values.count() { it.toInt() == 2 } == 2) {
-            "double"
+            if (cards.contains('J')) {
+               if (cardAmts['J']!!.toInt() == 2) return "quadruple"
+               else return "full"
+            }
+
+            return "double"
         } else if (cardAmts.values.contains(2)) {
-            "single"
+            if (cards.contains("J")) return "triple"
+
+            return "single"
         } else {
-            "high"
+            if (cards.contains("J")) return "single"
+
+            return "high"
         }
     }
 
@@ -59,8 +75,9 @@ fun main() {
             val cards = line.substringBefore(" ")
             val bidAmt = line.substringAfter(" ").toLong()
             val handType = determineHandType(cards)
+            println(handType)
 
-            hands.add(Hand(cards, bidAmt, handType))
+            hands.add(Hand(cards, bidAmt, handType, cards.replace('J', 'X')))
         }
 
         return hands
@@ -87,6 +104,28 @@ fun main() {
         return result
     }
 
+    fun part2(input: List<String>): Long {
+        var result: Long = 0
+
+        val hands: MutableList<Hand> = inputToHands(input)
+
+        hands.sortWith(compareBy<Hand>
+            { pointMapping[it.handType] }.thenBy
+            { pointMapping[it.originalCards[0].toString()] }.thenBy
+            { pointMapping[it.originalCards[1].toString()] }.thenBy
+            { pointMapping[it.originalCards[2].toString()] }.thenBy
+            { pointMapping[it.originalCards[3].toString()] }.thenBy
+            { pointMapping[it.originalCards[4].toString()] }
+        )
+
+        for (handIndex in hands.indices) {
+            result += (handIndex + 1).toLong() * hands[handIndex].bidAmt
+        }
+
+        return result
+    }
+
     val input = readInput("Day07")
-    part1(input).println()
+    // part1(input).println()
+    part2(input).println()
 }
